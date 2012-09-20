dbe570f6ffe7b48e818020bdaccd84f01c279990
Miguel Bermudez <miguel@miguelbermduez.com>
2012-09-20 03:20:20 -0400

    images drawing according to sort with swatch and text layered on top

--- src/huesort/HueSort.java
+++ src/huesort/HueSort.java
@@ -12,10 +12,6 @@ import java.io.File;
 import java.util.*;
 import java.util.Map.Entry;
 
-import javax.media.jai.Histogram;
-
-import com.sun.org.apache.xalan.internal.xsltc.runtime.Hashtable;
-
 import toxi.color.*;
 import toxi.color.theory.*;
 import toxi.math.*;
@@ -24,19 +20,20 @@ import toxi.math.*;
 
 public class HueSort extends PApplet {
 	
-	int IMG_NUM = 3;
-	int IMG_SAMPLES = 200;
+	int IMG_NUM = 90; //DEBUGGING, set to 0 to use all images in text file
+	int IMG_SAMPLES = 1000;
 	Float IMG_TOLERANCE = 0.10f;
-	boolean BATCH_MODE = true;
+	Boolean BATCH_MODE = true;
 	String LIST_OF_IMAGES = "data/artwork.txt";
 	String SINGLE_IMAGE = "data/artwork/1x/DT45.jpg";
 	
 	PImage a;
-	HashMap<String, Float> imageHues = new HashMap<String, Float>();
+	HashMap<String, TColor> imageHues = new HashMap<String, TColor>();
+	Map<String, TColor> sortedimageHues;
 	
 
 	public void setup() {
-		size(600,600);
+		size(1400,800);
 		smooth();
 		
 		if (BATCH_MODE) {
@@ -45,7 +42,8 @@ public class HueSort extends PApplet {
 			if(f.exists()) {
 				String[] imageList = loadStrings(f);
 				if (imageList.length > 0 ) {
-					for (int i = 0; i < IMG_NUM; i++) {
+					int count = IMG_NUM == 0 ? imageList.length : IMG_NUM;
+					for (int i = 0; i < count; i++) {
 						//println(imageList[i].trim()); //DEBUGGING		
 						processNewImage(imageList[i].trim());
 					}
@@ -58,8 +56,12 @@ public class HueSort extends PApplet {
 		}
 		
 		sortHueHash();
+		drawPImages();
 		
+		println ("\nUsing SAMPLES: " + IMG_SAMPLES + " TOLERANCE: " + IMG_TOLERANCE);
 		noLoop();
+		
+		
 	}
 
 	public void draw() {
@@ -77,19 +79,9 @@ public class HueSort extends PApplet {
 	void processNewImage(String imageFileName) {
 		PImage a;
 		a = loadImage(imageFileName);
-		image(a, 0, 0);
 		toxi.color.Histogram hist = toxi.color.Histogram.newFromARGBArray(a.pixels, IMG_SAMPLES, IMG_TOLERANCE, false);
+		
 		List<HistEntry> aHistEntries = hist.getEntries();
-
-		/*
-		int p =0;
-		for (Iterator iterator = aHistEntries.iterator(); iterator.hasNext();) {
-			HistEntry histEntry = (HistEntry) iterator.next();
-			TColor t = histEntry.getColor();
-			println(p + ":  Color: " + histEntry.getColor().toHex()+ "\t\t" + histEntry.getFrequency()); //DEBUGGING
-			p++;
-		}
-		*/
 		
 		//fist element of histogram is the most frequent color
 		toxi.color.HistEntry h = aHistEntries.get(0);
@@ -97,7 +89,7 @@ public class HueSort extends PApplet {
 		//println("Dominant Hue: " + t.hue() + "\t Color: " + t.toHex() + "\t file: " + imageFileName); //DEBUGGING
 		
 		//add to hues hashmap
-		imageHues.put(imageFileName, t.hue());
+		imageHues.put(imageFileName, t);
 	}
 	
 	
@@ -108,23 +100,69 @@ public class HueSort extends PApplet {
 	 */
 	void sortHueHash() {
 		// http://stackoverflow.com/questions/10711413/sort-java-hashmap-by-values-using-on-log-n-complexity
+		//this is really ugly but couldn't figure out a cleaner way to sort a HashMap with a complex value
+		//instead of a just simple value
 		
-		List<Entry<String, Float>> entries 
-			= new ArrayList<Entry<String, Float>>(imageHues.entrySet());
+		List<Entry<String, TColor>> entries 
+			= new ArrayList<Entry<String, TColor>>((Collection<? extends Entry<String, TColor>>) imageHues.entrySet());
 		
-		Collections.sort(entries, new Comparator<Entry<String, Float>>() {
-			public int compare(Entry<String, Float> left, Entry<String, Float> right) {
-			    return right.getValue().compareTo(left.getValue());
+		Collections.sort(entries, new Comparator<Entry<String, TColor>>() {
+			public int compare(Entry<String, TColor> left, Entry<String, TColor> right) {
+				Float rightHue = right.getValue().hue();
+				Float leftHue = left.getValue().hue();
+				return rightHue.compareTo(leftHue);
 			}
 		});
 
-		Map<String, Float> sortedMap = new LinkedHashMap<String, Float>(entries.size());
+		sortedimageHues= new LinkedHashMap<String, TColor>(entries.size());
 		
-		for (Entry<String, Float> entry : entries) {
-		  sortedMap.put(entry.getKey(), entry.getValue());
+		for (Entry<String, TColor> entry : entries) {
+			sortedimageHues.put(entry.getKey(), entry.getValue());
 		}
 		
-		println(sortedMap.toString()); //DEBUGGING
+		//println(sortedimageHues.toString()); //DEBUGGING
+	}
+	
+	/**
+	 *  
+	 * Draw Images to screen with color swatch on top
+	 * 
+	 */
+	void drawPImages() {
+		int widthCounter = 0;
+		int lineCounter = 0;
+		int maxHeight = 0;
+		int swatchSize = 30;
+		
+		int i = 0;
+		for (Entry<String, TColor> entry : sortedimageHues.entrySet())
+		{		    
+			String file = entry.getKey(); 
+			PImage p = loadImage(file);
+			TColor c = entry.getValue();
+			
+			p.resize(0, 100);
+			maxHeight = max(p.height, maxHeight);
+			if (widthCounter + p.width >= width) {
+				widthCounter = 0;
+				lineCounter += maxHeight;
+				maxHeight = 0;
+			}
+			
+			image(p, widthCounter, lineCounter);
+			fill(c.toARGB());
+			stroke(TColor.WHITE.toARGB());
+			//rect(widthCounter, lineCounter+p.height-20, 20, 20);
+			rect(widthCounter, lineCounter, swatchSize, swatchSize);
+			fill(TColor.WHITE.toARGB());
+			text(c.hue(), widthCounter, lineCounter+(1.5f*swatchSize));
+			println(i + ": " + file + "\t\tHue: " + c.hue() 
+					+ "\tColor: "+ c.toHex()); 
+			    
+			widthCounter+=p.width;
+			i++;
+					  
+		}
 	}
 	
 	
