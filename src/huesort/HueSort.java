deb66abe5356243362c63a4649c0591841b6e8d6
Miguel Bermudez <miguel@miguelbermduez.com>
2012-09-20 00:16:33 -0400

    working histogram

--- src/huesort/HueSort.java
+++ src/huesort/HueSort.java
@@ -6,37 +6,84 @@
 
 package huesort;
 
-import processing.core.PApplet;
-import processing.core.PImage;
+import processing.core.*;
 
 import java.io.File;
 import java.util.*;
 
+import javax.media.jai.Histogram;
+
 import toxi.color.*;
+import toxi.color.theory.*;
+import toxi.math.*;
+
 
 
 public class HueSort extends PApplet {
 	
-	String listOfImages = "data/artwork.txt";
+	int IMG_NUM = 0;
+	int IMG_SAMPLES = 100;
+	float IMG_TOLERANCE = 0.20f;
+	boolean BATCH_MODE = false;
+	String LIST_OF_IMAGES = "data/artwork.txt";
+	String SINGLE_IMAGE = "data/artwork/1x/DT45.jpg";
+	
 	PImage a;
+	
 
 	public void setup() {
-		File f = new File(listOfImages);
-		println("listOfImages: " + listOfImages);
-		if(f.exists()) {
-			String[] imageList = loadStrings(f);
-			if (imageList.length > 0 ) {
-				for (int i = 0; i < imageList.length; i++) {
-					println(imageList[i].trim());
+		size(600,600);
+		smooth();
+		
+		if (BATCH_MODE) {
+			File f = new File(LIST_OF_IMAGES);
+			//println("listOfImages: " + listOfImages); //DEBUGGING
+			if(f.exists()) {
+				String[] imageList = loadStrings(f);
+				if (imageList.length > 0 ) {
+					for (int i = 0; i < IMG_NUM; i++) {
+						//println(imageList[i].trim()); //DEBUGGING		
+						processNewImage(imageList[i].trim());
+					}
 				}
+			} else {
+				println("*** MISSING " + LIST_OF_IMAGES + " ****");
 			}
 		} else {
-			println("*** MISSING " + listOfImages + " ****");
+			processNewImage(SINGLE_IMAGE);
 		}
 		
 		noLoop();
 	}
 
 	public void draw() {
+	}
+	
+	
+	
+	/**
+	 *  
+	 * Begins the new load of each each and subsequent color data processing.
+	 * 
+	 * @param String imageFileName
+	 */
+	@SuppressWarnings("unused")
+	void processNewImage(String imageFileName) {
+		PImage a;
+		a = loadImage(imageFileName);
+		image(a, 0, 0);
+		//ColorList aColorList = ColorList.createFromARGBArray(a.pixels, IMG_SAMPLES, false);
+		toxi.color.Histogram hist = toxi.color.Histogram.newFromARGBArray(a.pixels, IMG_SAMPLES, IMG_TOLERANCE, false);
+		//hist.compute(IMG_TOLERANCE, false);
+		List<HistEntry> aHistEntries = hist.getEntries();
+		int p =0;
+		for (Iterator iterator = aHistEntries.iterator(); iterator.hasNext();) {
+			HistEntry histEntry = (HistEntry) iterator.next();
+			println(p + ":  Color: " + histEntry.getColor().toHex()+ "\t\t" + histEntry.getFrequency());
+			
+			p++;
+		}
+		
+		
 	}
 }
